ff36047ee26052dde251484246111c7dca95ead5
Bradford D. Boyle <bradfordb@vmware.com>
2020-06-19 10:44:10 -0700

    Conditionally set $PYTHONHOME only when vendoring python
    Setting `$PYTHONHOME` for releases where python is not vendored causes
    the system python to fail with an error message about not being able to
    find the site module.
    
    This commit updates the logic in generate-greenplum_path.sh to check if
    we are vendoring python and if we are, it includes setting `$PYTHONHOME`
    in greenplum_path.sh. If we are vendoring python, then at build-time
    `$PYTHONHOME` is set to point at our vendored python.
    
    [#173046174]
    
    Authored-by: Bradford D. Boyle <bradfordb@vmware.com>

--- gpMgmt/bin/generate-greenplum-path.sh
+++ gpMgmt/bin/generate-greenplum-path.sh
@@ -4,13 +4,22 @@ cat <<"EOF"
 #!/usr/bin/env bash
 GPHOME="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-PYTHONHOME="${GPHOME}/ext/python"
-PYTHONPATH="${GPHOME}/lib/python"
-PATH="${GPHOME}/bin:${PYTHONHOME}/bin:${PATH}"
-LD_LIBRARY_PATH="${GPHOME}/lib:${PYTHONHOME}/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 EOF
 
+if [ -x "${PYTHONHOME}/bin/python" ]; then
+	cat <<-"EOF"
+	PYTHONHOME="${GPHOME}/ext/python"
+	export PYTHONHOME
+
+	PATH="${PYTHONHOME}/bin:${PATH}"
+	LD_LIBRARY_PATH="${PYTHONHOME}/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+	EOF
+fi
+
 cat <<"EOF"
+PYTHONPATH="${GPHOME}/lib/python"
+PATH="${GPHOME}/bin:${PATH}"
+LD_LIBRARY_PATH="${GPHOME}/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 
 if [ -e "${GPHOME}/etc/openssl.cnf" ]; then
 	OPENSSL_CONF="${GPHOME}/etc/openssl.cnf"
@@ -18,7 +27,6 @@ fi
 
 export GPHOME
 export PATH
-export PYTHONHOME
 export PYTHONPATH
 export LD_LIBRARY_PATH
 export OPENSSL_CONF
