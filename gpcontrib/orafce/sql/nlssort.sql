8c4f58f4efa99b71ef1ed3d3c2c44b81c23d7cef
Brent Doil <bdoil@vmware.com>
2021-03-01 02:54:33 -0500

    Photon testing: en_US.utf8 is not a valid LC_COLLATE value.
    - gpinitsystem will query the system for the correct utf-8 variation to use as the default, and pass it to initdb.
    - Use the UTF8 encoding of template0 to test nlssort.
    - When changing from default, use 'C' since it is available on all POSIX compliant systems.
    
    Authored-by: Brent Doil <bdoil@vmware.com>

--- gpcontrib/orafce/sql/nlssort.sql
+++ gpcontrib/orafce/sql/nlssort.sql
@@ -5,20 +5,20 @@
 -- end_matchsubs
 SET client_min_messages = error;
 DROP DATABASE IF EXISTS regression_sort;
-CREATE DATABASE regression_sort WITH TEMPLATE = template0 ENCODING='UTF-8' LC_COLLATE='C' LC_CTYPE='C';
+CREATE DATABASE regression_sort WITH TEMPLATE = template0 ENCODING='UTF-8';
 \c regression_sort
 SET client_min_messages = error;
 CREATE EXTENSION orafce;
 SET client_min_messages = default;
 CREATE TABLE test_sort (name TEXT);
 INSERT INTO test_sort VALUES ('red'), ('brown'), ('yellow'), ('Purple');
-SELECT * FROM test_sort ORDER BY NLSSORT(name, 'en_US.utf8');
 SELECT * FROM test_sort ORDER BY NLSSORT(name, '');
+SELECT * FROM test_sort ORDER BY NLSSORT(name, 'C');
 SELECT set_nls_sort('invalid');
 SELECT * FROM test_sort ORDER BY NLSSORT(name);
-SELECT set_nls_sort('');
+SELECT set_nls_sort('C');
 SELECT * FROM test_sort ORDER BY NLSSORT(name);
-SELECT set_nls_sort('en_US.utf8');
+SELECT set_nls_sort('');
 SELECT * FROM test_sort ORDER BY NLSSORT(name);
 INSERT INTO test_sort VALUES(NULL);
 SELECT * FROM test_sort ORDER BY NLSSORT(name);
